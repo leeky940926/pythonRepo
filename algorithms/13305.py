1b505a3c6f7e56640358bdebd76721950093bd5f
kylee <leeky940926@gmail.com>
2022-04-09 15:28:53 +0900

    13305 수정 및 회고파일 생성

--- algorithms/13305.py
+++ algorithms/13305.py
@@ -1,10 +1,20 @@
-n = int(input("n : "))
-l = []
-p = []
+n = int(input())
+l = list(map(int, input().split()))
+p = list(map(int, input().split()))
+
+# for i in range(n-1):
+#     l.append(int(input()))
+
+# for i in range(n):
+#     p.append(int(input()))
+    
+a=0
+cost = p[0]
 
 for i in range(n-1):
-    l.append(int(input()))
+    if cost > p[i]:
+        cost = p[i]
+    a += cost * l[i]
 
-for i in range(n):
-    p.append(int(input()))
-
+print(a)
+    
