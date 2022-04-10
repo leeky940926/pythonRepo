ff62898a579485ff68c7432d16f83fcc74bdecb0
kylee <leeky940926@gmail.com>
2022-04-10 17:34:31 +0900

    백준 회고

--- algorithms/돌아보기1.py
+++ algorithms/돌아보기1.py
@@ -56,15 +56,92 @@
 '''
 1931
 '''
-n = int(input())
+# n = int(input())
+# l=list()
+
+# for i in range(n):
+#     l.append(list(map(int, input().split())))
+
+# l = sorted(l, key=lambda a:a[0])
+# l = sorted(l, key=lambda a:a[1])
+    
+# cnt=0
+# base = 0
+
+# for i,j in l:
+#     if i >= base:
+#         base = j
+#         cnt+=1
+
+# print(cnt)
+
+'''
+1026
+'''
+# n=int(input())
+
+# a = list(map(int, input().split()))
+# b = list(map(int, input().split()))
+# r=0
+
+# a.sort()
+
+# while n>0:
+#     r += min(a) * max(b)
+#     a.remove(min(a))
+#     b.remove(max(b))
+#     n-=1
+
+# print(r)
+
+'''
+1541
+'''
+# n = input().split("-")
+# l = list()
+# for i in n:
+#     r=0
+#     a = i.split("+")
+#     for j in a:
+#         r+= int(j)
+#     l.append(r)
+
+# c = l[0] * 2 - sum(l)
+# print(c)
+
+'''
+5585
+'''
+# n = int(input())
+# n=1000-n
+
+# l = [500, 100, 50, 10, 5, 1]
+
+# a=0
+
+# for i in l:
+#     a += n//i
+#     n -= i*(n//i)
+
+# print(a)
+
+'''
+2217
+'''
+n=int(input())
+
 l=list()
+m=list()
+
+a=0
 
 for i in range(n):
-    l.append(list(map(int, input().split())))
+    l.append(int(input()))
     
-cnt=1
+    #35 33 30 20 12
+l.sort(reverse=True)
 
-l = sorted(l, key=lambda a:a[0])
-#l = sorted(l, key=lambda a:a[1])
+for i,v in enumerate(l):
+    m.append((i+1)*v)
 
-print(l)
+print(max(m))
